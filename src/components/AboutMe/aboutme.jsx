6957ea9f553845643339245249af0c60bbbc40af
sedamer <mrcnsedaa@gmail.com>
2024-05-23 01:06:03 +0300

    myPortfolio

--- src/components/AboutMe/aboutme.jsx
+++ src/components/AboutMe/aboutme.jsx
@@ -1,5 +1,6 @@
 import "./aboutme.scss";
 
+import cap from "../../assets/graduation-cap-variant.png";
 import graduate from "../../assets/diploma.png";
 import { useTranslation } from "react-i18next";
 import work from "../../assets/customer.png";
@@ -26,43 +27,82 @@ export default function AboutMe() {
       <div className="about__content__right">
         <div className="about__content__right__section">
           <div className="about__content__right__section__edu">
-            <p style={{ color: "#9B2042", marginTop: "10px" }}>
+            <p
+              style={{
+                color: "#b38a9d",
+                marginTop: "20px",
+                fontSize: "2.1rem",
+                letterSpacing: "2px",
+                fontStyle: "italic",
+                fontFamily: "SourceSans3-ExtraLightItalic",
+              }}
+            >
               {t("Education")}
               <img src={graduate} alt="graduate" />
             </p>
             <ul className="about__content__right__section__edu">
               <li>
-                {t("Computer engineering")}
-                <p>
+                <p style={{ color: "black", fontSize: "17px" }}>
+                  {t("Computer Engineering")}
+                </p>
+                <p style={{ color: "gray", fontSize: "15px" }}>
                   <i>
                     {t("Adana Alparslan Türkeş Science and Technology")}
                     <br></br> {t("University")}
                   </i>
                 </p>
               </li>
-              <p> 3.20 | 4.00</p>
-              <p>2019-2024</p>
+              <p style={{ color: "gray", fontSize: "15px" }}> 3.20 | 4.00</p>
+              <p
+                style={{ color: "gray", fontSize: "15px", marginRight: "50px" }}
+              >
+                <img
+                  src={cap}
+                  alt="cap"
+                  style={{ marginRight: "12px", marginBottom: "-15px" }}
+                />
+                2019-2024
+              </p>
             </ul>
           </div>
+          <div className="about__content__right__section__dot"></div>
           <div className="about__content__right__section__edu">
-            <p style={{ color: "#9b8588" }}>
+            <p
+              style={{
+                color: "#b38a9d",
+                marginTop: "20px",
+                fontSize: "2.1rem",
+                letterSpacing: "2px",
+                fontStyle: "italic",
+                fontFamily: "SourceSans3-ExtraLightItalic",
+              }}
+            >
               {t("Experience")}
               <img src={work} alt="work" />
             </p>
             <ul className="about__content__right__section__edu">
               <li>
-                {t("Front-end developer Intern")}
-                <p>
+                <p style={{ color: "black", fontSize: "17px" }}>
+                  {t("Front-end Developer Intern")}
+                </p>
+                <p style={{ color: "gray", fontSize: "15px" }}>
                   <i>Brew Interactive</i>
                 </p>
-                <p>02/2024 - present</p>
+                <p style={{ color: "gray", fontSize: "15px" }}>
+                  02/2024 - present
+                </p>
               </li>
               <li>
-                {t("Computer Engineering Intern")}
-                <p>
-                  <i>{t("adana")}</i>
+                <p style={{ color: "black", fontSize: "17px" }}>
+                  {" "}
+                  {t("Computer Engineering Intern")}
                 </p>
-                <p>02/2023 -08/2023 </p>
+                <p style={{ color: "gray", fontSize: "15px" }}>
+                  <i>{t("Adana Büyükşehir Belediyesi")}</i>
+                </p>
+                <p style={{ color: "gray", fontSize: "15px" }}>
+                  02/2023 -08/2023{" "}
+                </p>
               </li>
             </ul>
           </div>
